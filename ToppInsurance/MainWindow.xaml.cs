88399ce1d3d05d04981642da7a5cc1929b7e70c0
BeataJacobsson <153110166+S2205112@users.noreply.github.com>
2024-09-23 13:37:48 +0200

    Login widget

--- ToppInsurance/MainWindow.xaml.cs
+++ ToppInsurance/MainWindow.xaml.cs
@@ -8,17 +8,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
-namespace ToppInsurance
+namespace LoginPrototyp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
-            InitializeComponent();
+
         }
     }
 }
