b460bb239f2ae84a7476edb6d5ed123c876b21f4
Clarahanssson <clarahansson123@gmail.com>
2024-09-23 16:32:18 +0200

    RegisterNewBusinessCustomer

---
+++ ToppInsurance/ViewModels/RegisterPrivateCustomerVM.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopInsuranceWPF.Commands;
+
+namespace TopInsuranceWPF.ViewModels
+{
+    public class RegisterPrivateCustomerVM 
+    {
+
+        //#region Properties Add privatecustomer
+
+        //private string _newName;
+        //public string NewName
+        //{
+        //    get { return _newName; }
+        //    set
+        //    {
+        //        if (_newName != value)
+        //        {
+        //            _newName = value;
+        //            OnPropertyChanged(NewName);
+        //        }
+        //    }
+        //}
+
+        //private string _newPhoneNumber;
+        //public string NewPhoneNumber
+        //{
+        //    get { return _newPhoneNumber; }
+        //    set
+        //    {
+        //        if (_newPhoneNumber != value)
+        //        {
+        //            _newPhoneNumber = value;
+        //            OnPropertyChanged(NewPhoneNumber);
+        //        }
+        //    }
+        //}
+
+        //private string _newEmailAddress;
+        //public string NewEmailAddress
+        //{
+        //    get { return _newEmailAddress; }
+        //    set
+        //    {
+        //        if (_newEmailAddress != value)
+        //        {
+        //            _newEmailAddress = value;
+        //            OnPropertyChanged(NewEmailAddress);
+        //        }
+        //    }
+        //}
+
+        //private string _newAddress;
+        //public string NewAddress
+        //{
+        //    get { return _newAddress; }
+        //    set
+        //    {
+        //        if (_newAddress != value)
+        //        {
+        //            _newAddress = value;
+        //            OnPropertyChanged(NewAddress);
+        //        }
+        //    }
+        //}
+
+        //private string _newZipcode;
+        //public string NewZipcode
+        //{
+        //    get { return _newZipcode; }
+        //    set
+        //    {
+        //        if (_newZipcode != value)
+        //        {
+        //            _newZipcode = value;
+        //            OnPropertyChanged(NewZipcode);
+        //        }
+        //    }
+        //}
+
+        //private string _newCity;
+        //public string NewCity
+        //{
+        //    get { return _newCity; }
+        //    set
+        //    {
+        //        if (_newCity != value)
+        //        {
+        //            _newCity = value;
+        //            OnPropertyChanged(NewCity);
+        //        }
+        //    }
+        //}
+
+        //private string _newSSN;
+        //public string NewSSN
+        //{
+        //    get { return _newSSN; }
+        //    set
+        //    {
+        //        if (_newSSN != value)
+        //        {
+        //            _newSSN = value;
+        //            OnPropertyChanged(NewSSN);
+        //        }
+        //    }
+        //}
+
+        //private string _newWorkPhoneNumber;
+        //public string NewWorkPhoneNumber
+        //{
+        //    get { return _newWorkPhoneNumber; }
+        //    set
+        //    {
+        //        if (_newWorkPhoneNumber != value)
+        //        {
+        //            _newWorkPhoneNumber = value;
+        //            OnPropertyChanged(NewWorkPhoneNumber);
+        //        }
+        //    }
+        //}
+
+        //#endregion
+
+    }
+}
