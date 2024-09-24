be71462ee23511c974b30ec1e349a5005d3deae9
Clarahanssson <clarahansson123@gmail.com>
2024-09-24 15:22:23 +0200

    Controller

---
+++ ToppInsuranceDL/BusinessRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToppInsuranceEntities;
+
+namespace TopInsuranceDL
+{
+    public class BusinessRepository
+    {
+        private UnitOfWork unitOfWork;
+        public BusinessRepository()
+        {
+            unitOfWork = UnitOfWork.GetInstance();
+        }
+
+        #region Register new business customer Method
+        public void CreateNewBusinessCustomer(string companyName, int organizationalNumber, int countryCode,
+                          string name, int phoneNumber, string emailAddress, string address,
+                          int zipCode, string city)
+        {
+            BusinessCustomer businessCustomer = new BusinessCustomer(companyName, organizationalNumber, countryCode,
+                          name, phoneNumber, emailAddress, address,
+                          zipCode, city);
+            //unitOfWork.BusinessRepository.Add(businessCustomer);
+            unitOfWork.Save();
+        }
+        #endregion
+
+        #region Patient peronalnumber unique Method
+        //public bool IsOrganizationalnumberUnique(string organizationalnumber)
+        //{
+        //    bool isUnique = !unitOfWork.BusinessRepository.Any(p => p.Organizationalnumber == organizationalnumber);
+        //    return isUnique;
+        //}
+        #endregion
+
+    }
+}
---
+++ ToppInsuranceDL/PrivateRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToppInsuranceEntities;
+
+namespace TopInsuranceDL
+{
+    public class PrivateRepository
+    {
+        private UnitOfWork unitOfWork;
+        public PrivateRepository()
+        {
+            unitOfWork = UnitOfWork.GetInstance();
+        }
+
+        #region Register new private customer Method
+        public void CreateNewPrivateCustomer(string ssn, int workPhonenumber, string name, int phoneNumber, 
+            string emailAddress, string address, int zipCode, string city)
+        {
+            PrivateCustomer privateCustomer = new PrivateCustomer(ssn, workPhonenumber, name, phoneNumber, emailAddress, address, zipCode, city);
+            //unitOfWork.PrivateRepository.Add(privateCustomer);
+            unitOfWork.Save();
+        }
+        #endregion
+    }
+}
