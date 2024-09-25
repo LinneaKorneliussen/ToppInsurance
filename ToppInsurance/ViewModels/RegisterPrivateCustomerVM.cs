532a0ac955967f47e7428affdfab563c76250b36
LinneaKorneliussen <153073744+LinneaKorneliussen@users.noreply.github.com>
2024-09-25 10:41:53 +0200

    Testing

--- ToppInsurance/ViewModels/RegisterPrivateCustomerVM.cs
+++ ToppInsurance/ViewModels/RegisterPrivateCustomerVM.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TopInsuranceBL;
+﻿using TopInsuranceBL;
 using TopInsuranceWPF.Commands;
 
 namespace TopInsuranceWPF.ViewModels
