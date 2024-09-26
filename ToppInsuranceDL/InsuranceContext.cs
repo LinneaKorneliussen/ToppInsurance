2589321beb904aebdc8572f04af60802cea597a0
BeataJacobsson <153110166+S2205112@users.noreply.github.com>
2024-09-26 12:48:06 +0200

    Reveal password

--- ToppInsuranceDL/InsuranceContext.cs
+++ ToppInsuranceDL/InsuranceContext.cs
@@ -66,7 +66,7 @@ namespace TopInsuranceDL
             //// Business Customer 
             //BCustomers.Add(new BusinessCustomer("Sven Göransson", "0768792345", "Sven@icabanken.se", "Ica vägen 6", 76890, "Borås", "Ica banken", 89020201, 46));
 
-            SaveChanges();
+            //SaveChanges();
         }
     }
 }
