d9ae25c3e4dd5d5b6a3e4dc55de92514d14b1937
Celinalinnerblom <153074108+Celinalinnerblom@users.noreply.github.com>
2024-09-30 09:24:46 +0200

    Reg employer

---
+++ ToppInsuranceDL/EmployerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using TopInsuranceEntities;
+
+namespace TopInsuranceDL
+{
+    public class EmployerRepository
+    {
+        private UnitOfWork unitOfWork;
+        public EmployerRepository()
+        {
+            unitOfWork = UnitOfWork.GetInstance();
+        }
+    }
+}
