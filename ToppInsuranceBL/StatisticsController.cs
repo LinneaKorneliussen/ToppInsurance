37d753be76402ee697499bf688664c1d315b8e59
LinneaKorneliussen <153073744+LinneaKorneliussen@users.noreply.github.com>
2024-10-14 09:17:25 +0200

    Stistics frontend and backend

---
+++ ToppInsuranceBL/StatisticsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopInsuranceDL;
+using TopInsuranceEntities;
+
+namespace TopInsuranceBL
+{
+    public class StatisticsController
+    {
+        private StatisticsRepository statisticsRepository;
+
+        public StatisticsController()
+        {
+            statisticsRepository = new StatisticsRepository();
+        }
+
+        #region Get Salesperson by Last Name or Agency Number
+        public List<Employee> GetSalespersonsByLastNameOrAgencyNumber(string searchText)
+        {
+            return statisticsRepository.GetSalespersonsByLastNameOrAgencyNumber(searchText);
+        }
+        #endregion
+
+        #region Get Sales Data For Employee Method
+        public (Dictionary<string, List<int>> PrivateSalesData, Dictionary<string, List<int>> BusinessSalesData) GetSalesDataForEmployee(Employee employee)
+        {
+            return statisticsRepository.GetSalesDataForEmployee(employee);
+        }
+        #endregion
+    }
+}
