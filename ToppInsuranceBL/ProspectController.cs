49613304384a05fef19ccda275a71470bb12298f
Clarahanssson <clarahansson123@gmail.com>
2024-10-16 14:57:14 +0200

    CustomerProspect

---
+++ ToppInsuranceBL/ProspectController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopInsuranceDL;
+using TopInsuranceEntities;
+
+namespace TopInsuranceBL
+{
+    public class ProspectController
+    {
+        private ProspectRepository prospectRepository;
+
+        public ProspectController() 
+        {
+            prospectRepository = new ProspectRepository();
+        }
+
+        #region Add note
+        
+        public void AddPCNote(string note, Employee employee, PrivateCustomer? privateCustomer, BusinessCustomer? businessCustomer)
+        {
+            prospectRepository.AddPCNote(note, employee, privateCustomer, businessCustomer);
+        }
+
+        public void AddBCNote(string note, Employee employee, PrivateCustomer? privateCustomer, BusinessCustomer? businessCustomer)
+        {
+            prospectRepository.AddBCNote(note, employee, privateCustomer, businessCustomer);
+        }
+        #endregion
+
+
+
+    }
+}
