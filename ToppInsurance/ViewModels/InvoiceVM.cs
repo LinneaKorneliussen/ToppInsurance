15b66bda6047874b86ea18e87873f4e140477731
Celinalinnerblom <153074108+Celinalinnerblom@users.noreply.github.com>
2024-10-22 12:52:56 +0200

    Invoice xaml

---
+++ ToppInsurance/ViewModels/InvoiceVM.cs
@@ -0,0 +1,37 @@
+﻿using ControlzEx.Standard;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using TopInsuranceBL;
+using TopInsuranceDL;
+using TopInsuranceEntities;
+using TopInsuranceWPF.Commands;
+
+namespace TopInsuranceWPF.ViewModels
+{
+    public class InvoiceVM : ObservableObject
+    {
+        private BusinessController businessController;
+        private PrivateController privateController;
+    }
+
+    //public InvoiceVM()
+    //{
+    //    //businessController = new BusinessController();
+    //    //privateController = new PrivateController();
+    //    //ClearCommand = new RelayCommand(ClearFields);
+
+    //}
+
+
+    #region Properties
+
+
+
+
+    #endregion
+
+}
