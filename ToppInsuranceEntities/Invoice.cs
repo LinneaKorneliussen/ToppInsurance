a729c8643b7769e9f98b79d1f16cf642af01b213
BeataJacobsson <153110166+S2205112@users.noreply.github.com>
2024-10-22 13:20:16 +0200

    Faktura

---
+++ ToppInsuranceEntities/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInsuranceEntities
+{
+    public abstract class Invoice
+    {
+        public DateTime InvoiceDate { get; set; }
+        public double InvoiceTotalAmount { get; set; }
+
+    }
+}
---
+++ ToppInsuranceEntities/PrivateInvoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInsuranceEntities
+{
+    public class PrivateInvoice : Invoice
+    {
+        public PrivateCustomer PrivateCustomer { get; set; }
+
+
+    }
+}
